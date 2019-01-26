7d05eb9d31f146ce7990657874b5c5bfda6b7de8
Christian Dallago <code@dallago.us>
2019-01-26 17:29:42 +0100

    try to write sequence_id by reading alignment

--- prepare_runs.py
+++ prepare_runs.py
@@ -1,6 +1,7 @@
 import os
 import copy
 from pathlib import Path
+from evcouplings.align import read_fasta, parse_header
 from evcouplings.utils import read_config_file
 from evcouplings.utils.app import run_jobs, unroll_config
 
@@ -12,13 +13,17 @@ for alignment_file in alignment_files:
     alignment_path = os.path.join(alignments_folder, alignment_file)
     alignment_path = str(Path(alignment_path).resolve())
     alignment_name = os.path.splitext(alignment_file)[0]
+    alignment_binary = open(alignment_path)
+    alignment = read_fasta(alignment_binary)
+
+    first_sequence_id = parse_header(next(alignment)[0])
 
     directory = 'runs/{}'.format(alignment_name)
     current_config = copy.deepcopy(master_config)
     os.mkdir(directory)
 
     current_config['global']['prefix'] = directory
-    current_config['global']['sequence_id'] = alignment_name
+    current_config['global']['sequence_id'] = first_sequence_id
     current_config['align']['input_alignment'] = alignment_path
 
     conf = unroll_config(current_config)
